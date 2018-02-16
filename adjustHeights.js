c65361e290f1dced1ec615bb2ff77604c7f9ddee
fabianburghardt <fabianburghardt@Fabians-MacBook-Pro.local>
2018-02-16 15:45:16 +0100

    AdjustHeights.js

---
+++ adjustHeights.js
@@ -0,0 +1,24 @@
+// Execute Function with classname
+adjustHeights("same");
+
+// The Function
+function adjustHeights(el){
+  var _this = el;
+  var elements = document.getElementsByClassName(_this);
+  var maxHeight = 0;
+  for(var i = 0; i < elements.length; i++){
+    currentHeight = elements[i].offsetHeight;
+    if(currentHeight > maxHeight){
+      maxHeight = currentHeight;
+    }
+  }
+  for(var i = 0; i < elements.length; i++){
+    elements[i].style.height = maxHeight + "px";
+  }
+  window.onresize = function(){
+    for(var i = 0; i < elements.length; i++){
+      elements[i].style.height = "inherit";
+    }
+    setTimeout(function(){adjustHeights(_this);})
+  };
+}
